36545f1fb3277c434626fd4c0b40baa42a0c605e
atx <atx@umpecaa.com>
2025-02-01 00:04:10 +0000

    next iteration

--- code.py
+++ code.py
@@ -9,6 +9,8 @@ def create_deck():
     return deck
 
 def deal_card(deck):
+    if len(deck) == 0:
+        deck.extend(create_deck())
     return deck.pop()
 
 def calculate_hand(hand):
@@ -52,42 +54,79 @@ def get_player_choice():
         else:
             print("Invalid input. Please enter 'h' or 's'.")
 
+def get_bet(balance):
+    while True:
+        try:
+            bet = int(input(f"\nYou have ${balance}. Enter your bet: "))
+            if bet > 0 and bet <= balance:
+                return bet
+            else:
+                print(f"Invalid bet. Enter a positive number up to ${balance}.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
 def play_blackjack():
     deck = create_deck()
-    player_hand = [deal_card(deck), deal_card(deck)]
-    dealer_hand = [deal_card(deck), deal_card(deck)]
-    
-    while True:
-        display_hands(player_hand, dealer_hand, hide_dealer=True)
-        if calculate_hand(player_hand) == 21:
-            print("Blackjack! You win!")
-            return
-        choice = get_player_choice()
-        if choice == 'hit':
-            player_hand.append(deal_card(deck))
-            if calculate_hand(player_hand) > 21:
-                display_hands(player_hand, dealer_hand, hide_dealer=True)
-                print("You busted! Dealer wins.")
-                return
-        else:
+    balance = 100
+    print("Welcome to Blackjack!")
+    while balance > 0:
+        print(f"\nCurrent balance: ${balance}")
+        bet = get_bet(balance)
+        player_hand = [deal_card(deck), deal_card(deck)]
+        dealer_hand = [deal_card(deck), deal_card(deck)]
+        
+        while True:
+            display_hands(player_hand, dealer_hand, hide_dealer=True)
+            player_total = calculate_hand(player_hand)
+            if player_total == 21:
+                print("Blackjack! You win!")
+                balance += bet
+                break
+            choice = get_player_choice()
+            if choice == 'hit':
+                player_hand.append(deal_card(deck))
+                player_total = calculate_hand(player_hand)
+                if player_total > 21:
+                    display_hands(player_hand, dealer_hand, hide_dealer=True)
+                    print("You busted! Dealer wins.")
+                    balance -= bet
+                    break
+            else:
+                break
+        
+        if calculate_hand(player_hand) <= 21:
+            display_hands(player_hand, dealer_hand, hide_dealer=False)
+            while calculate_hand(dealer_hand) < 17:
+                dealer_hand.append(deal_card(deck))
+                display_hands(player_hand, dealer_hand, hide_dealer=False)
+                if calculate_hand(dealer_hand) > 21:
+                    print("Dealer busted! You win.")
+                    balance += bet
+                    break
+            else:
+                dealer_total = calculate_hand(dealer_hand)
+                player_total = calculate_hand(player_hand)
+                if dealer_total > player_total:
+                    print("Dealer wins.")
+                    balance -= bet
+                elif dealer_total < player_total:
+                    print("You win!")
+                    balance += bet
+                else:
+                    print("It's a tie!")
+        
+        if balance == 0:
+            print("You have no more money! Game over.")
             break
-    
-    display_hands(player_hand, dealer_hand, hide_dealer=False)
-    while calculate_hand(dealer_hand) < 17:
-        dealer_hand.append(deal_card(deck))
-        display_hands(player_hand, dealer_hand, hide_dealer=False)
-        if calculate_hand(dealer_hand) > 21:
-            print("Dealer busted! You win.")
-            return
-    
-    player_total = calculate_hand(player_hand)
-    dealer_total = calculate_hand(dealer_hand)
-    if dealer_total > player_total:
-        print("Dealer wins.")
-    elif dealer_total < player_total:
-        print("You win!")
-    else:
-        print("It's a tie!")
+        while True:
+            replay = input("\nDo you want to play again? (y/n): ").lower()
+            if replay in ['y', 'yes']:
+                break
+            elif replay in ['n', 'no']:
+                print(f"You leave the game with ${balance}.")
+                return
+            else:
+                print("Invalid input. Please enter 'y' or 'n'.")
 
 if __name__ == '__main__':
     play_blackjack()
