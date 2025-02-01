e0c19ae7759be4949b307d27a0b87196c7fdade1
atx <atx@umpecaa.com>
2025-02-01 00:04:48 +0000

    next iteration

--- code.py
+++ code.py
@@ -4,12 +4,13 @@ def create_deck():
     suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
     ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10',
              'J', 'Q', 'K', 'A']
-    deck = [{'suit': suit, 'rank': rank} for suit in suits for rank in ranks]
+    deck = [{'suit': suit, 'rank': rank} for suit in suits
+            for rank in ranks] * 6
     random.shuffle(deck)
     return deck
 
 def deal_card(deck):
-    if len(deck) == 0:
+    if len(deck) < 15:
         deck.extend(create_deck())
     return deck.pop()
 
@@ -44,15 +45,13 @@ def display_hands(player_hand, dealer_hand, hide_dealer=False):
         print(f"  {card['rank']} of {card['suit']}")
     print(f"Your hand value: {calculate_hand(player_hand)}")
 
-def get_player_choice():
+def get_player_choice(allowed):
     while True:
-        choice = input("\nDo you want to (h)it or (s)tand? ").lower()
-        if choice in ['h', 'hit']:
-            return 'hit'
-        elif choice in ['s', 'stand']:
-            return 'stand'
+        choice = input("\nChoose an action: ").lower()
+        if choice in allowed:
+            return allowed[choice]
         else:
-            print("Invalid input. Please enter 'h' or 's'.")
+            print("Invalid input. Please choose a valid action.")
 
 def get_bet(balance):
     while True:
@@ -67,34 +66,101 @@ def get_bet(balance):
 
 def play_blackjack():
     deck = create_deck()
-    balance = 100
+    balance = 1000
     print("Welcome to Blackjack!")
     while balance > 0:
         print(f"\nCurrent balance: ${balance}")
         bet = get_bet(balance)
         player_hand = [deal_card(deck), deal_card(deck)]
         dealer_hand = [deal_card(deck), deal_card(deck)]
-        
+        player_split = False
+        split_hands = []
+        current_hand = player_hand.copy()
         while True:
-            display_hands(player_hand, dealer_hand, hide_dealer=True)
-            player_total = calculate_hand(player_hand)
+            display_hands(current_hand, dealer_hand, hide_dealer=True)
+            player_total = calculate_hand(current_hand)
             if player_total == 21:
-                print("Blackjack! You win!")
-                balance += bet
+                print("Blackjack! You might win!")
                 break
-            choice = get_player_choice()
+            actions = {'h': 'hit', 's': 'stand'}
+            if len(current_hand) == 2 and balance >= bet:
+                actions['d'] = 'double down'
+                if current_hand[0]['rank'] == current_hand[1]['rank']:
+                    actions['p'] = 'split'
+            choice = get_player_choice(actions)
             if choice == 'hit':
-                player_hand.append(deal_card(deck))
-                player_total = calculate_hand(player_hand)
+                current_hand.append(deal_card(deck))
+                player_total = calculate_hand(current_hand)
                 if player_total > 21:
-                    display_hands(player_hand, dealer_hand, hide_dealer=True)
+                    display_hands(current_hand, dealer_hand, hide_dealer=True)
                     print("You busted! Dealer wins.")
                     balance -= bet
                     break
-            else:
+            elif choice == 'stand':
                 break
-        
-        if calculate_hand(player_hand) <= 21:
+            elif choice == 'double down':
+                additional_bet = bet
+                if additional_bet > balance:
+                    print("Insufficient balance to double down.")
+                    continue
+                bet += additional_bet
+                current_hand.append(deal_card(deck))
+                player_total = calculate_hand(current_hand)
+                display_hands(current_hand, dealer_hand, hide_dealer=True)
+                if player_total > 21:
+                    print("You busted! Dealer wins.")
+                    balance -= bet
+                    break
+                else:
+                    break
+            elif choice == 'split':
+                split_hands = [
+                    [current_hand[0], deal_card(deck)],
+                    [current_hand[1], deal_card(deck)]
+                ]
+                balance -= bet
+                player_split = True
+                break
+        if player_split:
+            for hand in split_hands:
+                current_hand = hand
+                print("\nPlaying split hand:")
+                while True:
+                    display_hands(current_hand, dealer_hand, hide_dealer=True)
+                    player_total = calculate_hand(current_hand)
+                    if player_total == 21:
+                        print("Blackjack!")
+                        break
+                    actions = {'h': 'hit', 's': 'stand'}
+                    if len(current_hand) == 2 and balance >= bet:
+                        actions['d'] = 'double down'
+                    choice = get_player_choice(actions)
+                    if choice == 'hit':
+                        current_hand.append(deal_card(deck))
+                        player_total = calculate_hand(current_hand)
+                        if player_total > 21:
+                            display_hands(current_hand, dealer_hand, hide_dealer=True)
+                            print("You busted! Dealer wins.")
+                            balance -= bet
+                            break
+                    elif choice == 'stand':
+                        break
+                    elif choice == 'double down':
+                        additional_bet = bet
+                        if additional_bet > balance:
+                            print("Insufficient balance to double down.")
+                            continue
+                        bet += additional_bet
+                        current_hand.append(deal_card(deck))
+                        display_hands(current_hand, dealer_hand, hide_dealer=True)
+                        player_total = calculate_hand(current_hand)
+                        if player_total > 21:
+                            print("You busted! Dealer wins.")
+                            balance -= bet
+                            break
+                        else:
+                            break
+        if calculate_hand(player_hand) <= 21 and not player_split:
             display_hands(player_hand, dealer_hand, hide_dealer=False)
             while calculate_hand(dealer_hand) < 17:
                 dealer_hand.append(deal_card(deck))
@@ -114,8 +180,7 @@ def play_blackjack():
                     balance += bet
                 else:
                     print("It's a tie!")
-        
-        if balance == 0:
+        if balance <= 0:
             print("You have no more money! Game over.")
             break
         while True:
